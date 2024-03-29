07698347fe7950b360fd0b1e11e00565c19b5791
Cynthia-Matsitsa <cynthiamatsitsa@gmail.com>
2024-03-29 19:42:56 +0300

    add file

---
+++ function.js
@@ -0,0 +1,11 @@
+//Write a function that accepts an array of numbers and uses the forEach() method to console.log each number multiplied by 2
+let numbers = [4,8,5,6,8];
+let muitiply = [];
+number.forEach(function(number){
+    muitlplyby2.push(number*2);
+
+})
+console.log({multiplyby2});
+
+    
+
